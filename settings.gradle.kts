9f3816196961d36b5bc0e5e97e78eac8c117bf36
CATMIAOZHI <CATMIAOZHI@github.com>
2026-07-03 05:18:18 +0000

    fix: use official repositories on GitHub Actions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,30 +1,41 @@
 pluginManagement {
     repositories {
-        maven("https://maven.aliyun.com/repository/gradle-plugin")
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/public")
-        maven("https://repo.huaweicloud.com/repository/gradle-plugin/")
-        maven("https://repo.huaweicloud.com/repository/maven/")
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
+        if (System.getenv("GITHUB_ACTIONS") == "true") {
+            google()
+            mavenCentral()
+            gradlePluginPortal()
+        } else {
+            maven("https://maven.aliyun.com/repository/gradle-plugin")
+            maven("https://maven.aliyun.com/repository/google")
+            maven("https://maven.aliyun.com/repository/public")
+            maven("https://repo.huaweicloud.com/repository/gradle-plugin/")
+            maven("https://repo.huaweicloud.com/repository/maven/")
+            google {
+                content {
+                    includeGroupByRegex("com\\.android.*")
+                    includeGroupByRegex("com\\.google.*")
+                    includeGroupByRegex("androidx.*")
+                }
             }
+            mavenCentral()
+            gradlePluginPortal()
         }
-        mavenCentral()
-        gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven("https://maven.aliyun.com/repository/google")
-        maven("https://maven.aliyun.com/repository/central")
-        maven("https://maven.aliyun.com/repository/public")
-        maven("https://repo.huaweicloud.com/repository/maven/")
-        google()
-        mavenCentral()
+        if (System.getenv("GITHUB_ACTIONS") == "true") {
+            google()
+            mavenCentral()
+        } else {
+            maven("https://maven.aliyun.com/repository/google")
+            maven("https://maven.aliyun.com/repository/central")
+            maven("https://maven.aliyun.com/repository/public")
+            maven("https://repo.huaweicloud.com/repository/maven/")
+            google()
+            mavenCentral()
+        }
     }
 }
 
